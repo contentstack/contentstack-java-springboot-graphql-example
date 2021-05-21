a5c4f96f9bf4232b5a318ec37813641eb3b226fd
shaileshmishra <Yes@383840>
2021-05-21 14:31:51 +0530

    :tada: initial commit

---
+++ src/main/java/com/contentstack/gqlspring/Util.java
@@ -0,0 +1,33 @@
+package com.contentstack.gqlspring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ResourceUtils;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ * The type Util.
+ */
+public class Util {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Util.class.getName());
+
+    /**
+     * Fetch product string.
+     *
+     * @return the string
+     */
+    public static String fetchProduct() {
+        String content = null;
+        try {
+            File file = ResourceUtils.getFile("classpath:product.graphql");
+            content = new String(Files.readAllBytes(file.toPath()));
+            LOGGER.debug("file: " + content);
+            return content;
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return content;
+    }
+}
---
+++ src/test/java/com/contentstack/gqlspring/GQLTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contentstack.gqlspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GQLTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
