e9d6276c199d4a3cf4eeba821273cbd2848d6df2
shaileshmishra <Yes@383840>
2021-05-31 13:52:47 +0530

    :hammer: refactored credentials

--- src/main/java/com/contentstack/gqlspring/GQLTestApplication.java
+++ src/main/java/com/contentstack/gqlspring/GQLTestApplication.java
@@ -2,10 +2,10 @@ package com.contentstack.gqlspring;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,13 +21,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api")
 public class GQLTestApplication {
 
+    private static String BASE_URL;
+    private static String DeliverToken;
+
     /**
      * The entry point of application.
      *
      * @param args the input arguments
      */
     public static void main(String[] args) {
+        loadEnvVar();
         SpringApplication.run(GQLTestApplication.class, args);
+    }
+
+
+    // load credentials from from .env
+    private static void loadEnvVar() {
+        Dotenv dotenv = Dotenv.load();
+        DeliverToken = dotenv.get("_EVV_DELIVERY_TOKEN");
+        String _API_KEY = dotenv.get("_ENV_API_KEY");
+        String _ENV = dotenv.get("_ENV");
+        BASE_URL = "https://graphql.contentstack.com/stacks/" + _API_KEY + "?environment=" + _ENV;
     }
 
 
@@ -37,14 +51,23 @@ public class GQLTestApplication {
      * @return the products
      * @throws JsonProcessingException the json processing exception
      */
-    @GetMapping(value = "/product")
+    @GetMapping(value = "/all_blog_post")
     public JsonNode getProducts() throws JsonProcessingException {
         GQL gqlInstance = GQL.Builder.newInstance()
-                .setUrl("https://graphql.contentstack.com/stacks/***REMOVED***?environment=production")
-                .setQueryString("{ all_product { items { title description } } }")
-                .setHeader("***REMOVED***")
+                .setUrl(BASE_URL)
+                .setQueryString("query{\n" +
+                        "  all_blog_post {\n" +
+                        "    items {\n" +
+                        "      date\n" +
+                        "      body\n" +
+                        "      title\n" +
+                        "      url\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "}\n")
+                .setHeader(DeliverToken)
                 .build();
-        JsonNode response = gqlInstance.fetch().get("data").get("all_product").get("items");;
+        JsonNode response = gqlInstance.fetch().get("data").get("all_blog_post").get("items");
         System.out.println(response);
         return response;
     }
@@ -56,21 +79,19 @@ public class GQLTestApplication {
      * @return the about
      * @throws JsonProcessingException the json processing exception
      */
-    @GetMapping(value = "/about")
+    @GetMapping(value = "/all_footer")
     public JsonNode getAbout() throws JsonProcessingException {
         GQL gqlInstance = GQL.Builder.newInstance()
-                .setTag("about")
-                .setUrl("https://graphql.contentstack.com/stacks/***REMOVED***?environment=development")
-                .setQueryString("query {\n" +
-                        "  home(uid: \"***REMOVED***\") {\n" +
-                        "    about {\n" +
+                .setTag("all_footer")
+                .setUrl(BASE_URL)
+                .setQueryString("{\n" +
+                        "  all_footer {\n" +
+                        "    items {\n" +
                         "      title\n" +
-                        "      left_description\n" +
-                        "      right_description\n" +
                         "    }\n" +
                         "  }\n" +
                         "}")
-                .setHeader("***REMOVED***")
+                .setHeader(DeliverToken)
                 .build();
         JsonNode response = gqlInstance.fetch();
         System.out.println(response);
@@ -84,27 +105,19 @@ public class GQLTestApplication {
      * @return the banner
      * @throws JsonProcessingException the json processing exception
      */
-    @GetMapping(value = "/banner")
+    @GetMapping(value = "/all_header")
     public JsonNode getBanner() throws JsonProcessingException {
         GQL gqlInstance = GQL.Builder.newInstance()
-                .setTag("banner")
-                .setUrl("https://graphql.contentstack.com/stacks/***REMOVED***?environment=development")
-                .setQueryString("query {\n" +
-                        "  home(uid: \"***REMOVED***\") {\n" +
-                        "    banner {\n" +
-                        "      description\n" +
+                .setTag("all_header")
+                .setUrl(BASE_URL)
+                .setQueryString("{\n" +
+                        "  all_header {\n" +
+                        "    items {\n" +
                         "      title\n" +
-                        "      imageConnection {\n" +
-                        "        edges {\n" +
-                        "          node {\n" +
-                        "            url\n" +
-                        "          }\n" +
-                        "        }\n" +
-                        "      }\n" +
                         "    }\n" +
                         "  }\n" +
-                        "}\n")
-                .setHeader("***REMOVED***")
+                        "}")
+                .setHeader(DeliverToken)
                 .build();
         JsonNode response = gqlInstance.fetch();
         System.out.println(response);
@@ -118,22 +131,31 @@ public class GQLTestApplication {
      * @return the contact
      * @throws JsonProcessingException the json processing exception
      */
-    @GetMapping(value = "/contact")
+    @GetMapping(value = "/all_page")
     public JsonNode getContact() throws JsonProcessingException {
         GQL gqlInstance = GQL.Builder.newInstance()
-                .setTag("banner")
-                .setUrl("https://graphql.contentstack.com/stacks/***REMOVED***?environment=development")
-                .setQueryString("query {\n" +
-                        "  home(uid: \"***REMOVED***\") {\n" +
-                        "    contact {\n" +
-                        "      address\n" +
-                        "      email\n" +
-                        "      phone_number\n" +
+                .setTag("all_page")
+                .setUrl(BASE_URL)
+                .setQueryString("{\n" +
+                        "  all_page {\n" +
+                        "    items {\n" +
+                        "      system {\n" +
+                        "        content_type_uid\n" +
+                        "        created_at\n" +
+                        "        created_by\n" +
+                        "        locale\n" +
+                        "        tags\n" +
+                        "        uid\n" +
+                        "        updated_at\n" +
+                        "        updated_by\n" +
+                        "        version\n" +
+                        "      }\n" +
                         "      title\n" +
+                        "      url\n" +
                         "    }\n" +
                         "  }\n" +
                         "}")
-                .setHeader("***REMOVED***")
+                .setHeader(DeliverToken)
                 .build();
         JsonNode response = gqlInstance.fetch();
         System.out.println(response);
--- src/test/java/com/contentstack/gqlspring/GQLTestApplicationTests.java
+++ src/test/java/com/contentstack/gqlspring/GQLTestApplicationTests.java
@@ -1,12 +1,10 @@
 package com.contentstack.gqlspring;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GQLTestApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
