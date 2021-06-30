394a6a072931f90a9dc7433dda1a955d08d1445a
shaileshmishra <Yes@383840>
2021-06-30 23:49:46 +0530

    :sparkles: Fresh UI added

--- src/test/java/com/contentstack/gqlspring/GQLTestApplicationTests.java
+++ src/test/java/com/contentstack/gqlspring/GQLTestApplicationTests.java
@@ -1,9 +1,11 @@
 package com.contentstack.gqlspring;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class GQLTestApplicationTests {
 
 	void contextLoads() {
 	}
-
 
 }
